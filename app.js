fe3c37f5b88daf147a68107fe3ccb9961f3990a6
reynoldsluke08@gmail.com <reynoldsluke08@gmail.com>
2016-10-26 19:01:47 +0100

    Auth partly complete and able to login and use token

--- app.js
+++ app.js
@@ -18,26 +18,3 @@ app.use(express.static(`${__dirname}/public`));
 app.use("/", router);
 
 app.listen(port, () => console.log(`Express started on port: ${port}`));
-
-
-
-// const express    = require("express");
-// const morgan     = require("morgan");
-// const bodyParser = require("body-parser");
-// const mongoose   = require("mongoose");
-// const expressJWT = require("express-jwt");
-//
-// const app        = express();
-// const port       = process.env.PORT || 3000;
-// const router     = require("./config/routes");
-//
-// mongoose.connect('mongodb://localhost/jwt-clientside');
-//
-// app.use(morgan("dev"));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(`${__dirname}/public`));
-//
-// app.use("/api", router);
-//
-// app.listen(port, () => console.log(`Express started on port: ${port}`));
--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,42 @@
 const router = require('express').Router();
-const usersControllers = require("../controllers/users");
+const jwt = require("jsonwebtoken");
+const secret = require('./tokens').secret;
 
+const usersControllers = require("../controllers/users");
+const authControllers = require("../controllers/auth");
+
+function secureRoute(req, res, next) {
+  if(!req.headers.authorization) return res.status(401).json({ message: "Unauthorized" });
+
+  let token = req.headers.authorization.replace('Bearer ', '');
+
+  jwt.verify(token, secret, (err, payload) => {
+    if(err) return res.status(401).json({message: "Unauthorized"});
+
+    req.user = payload;
+
+    next();
+
+  });
+}
+
+// router.route('/api/users')
+// .get(usersControllers.index)
+// .post(usersControllers.create);
+
+router.route("/register")
+  .post(authControllers.register);
+router.route("/login")
+  .post(authControllers.login);
 
 router.route('/api/users')
-.get(usersControllers.index)
-.post(usersControllers.create);
+  .all(secureRoute)
+  .get(usersControllers.index);
+router.route('/users/:id')
+  .all(secureRoute)
+  .post(usersControllers.create)
+  .get(usersControllers.show)
+  .put(usersControllers.update)
+  .delete(usersControllers.delete);
 
 module.exports = router;
